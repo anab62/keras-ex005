364151c7761b9b5e17c7a9f86156296d9451efd2
anab62 <anabusa62@gmail.com>
2023-09-28 13:40:13 +0900

    update on path info.

--- scripts/concat_mp4.bat
+++ scripts/concat_mp4.bat
@@ -1,7 +1,7 @@
 @echo off
 
-set FFMPEG="_internal\ffmpeg\ffmpeg.exe"
-set PYTHON="_internal\python-3.8.10\python.exe"
+set FFMPEG="..\_internal\ffmpeg\ffmpeg.exe"
+set PYTHON="..\_internal\python-3.8.10\python.exe"
 
 %FFMPEG% -f concat -i filelist.txt -c copy tmp_concat.mp4
 
--- scripts/partition_mp4_template.bat
+++ scripts/partition_mp4_template.bat
@@ -1,7 +1,7 @@
 @echo on
 
-set FFMPEG="_internal\ffmpeg\ffmpeg.exe"
-set PYTHON="_internal\python-3.8.10\python.exe"
+set FFMPEG="..\_internal\ffmpeg\ffmpeg.exe"
+set PYTHON="..\_internal\python-3.8.10\python.exe"
 
 echo "According to info. partition mp4 ..."
 
